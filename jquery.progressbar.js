633e0a591afecd51dfab3cd25abbdfed1b9b6f76
kelvinperrie <kelvinperrie@gmail.com>
2019-08-14 08:31:12 +1200

    getting this actually working

--- jquery.progressbar.js
+++ jquery.progressbar.js
@@ -1,39 +1,36 @@
-
-
 var ProgressBar = function(settings) {
     var self = this;
 
     self.element = settings.element;
     self.imageUrl = settings.imageUrl;
+    self.backgroundImageUrl = settings.backgroundImageUrl || settings.imageUrl;
     self.imageHeight = settings.imageHeight;
+    self.imageWidth = settings.imageWidth;
+    if (settings.backgroundOpacity || settings.backgroundOpacity === 0) {
+        self.backgroundOpacity = settings.backgroundOpacity;
+    } else {
+        self.backgroundOpacity = .3;
+    }
 
-    $(self.element).prepend($('<div>',{ class:'progress-bar' }));
-    $(self.element).children('.progress-bar').prepend($('<div>',{ class:'pb-background' }));
-    $(self.element).children('.progress-bar').prepend($('<div>',{ class:'pb-progress' }));
-    $(self.element).find('.pb-background').prepend($('<img/>',{ class:'background-image',src:settings.imageUrl }));
-    $(self.element).find('.pb-progress').prepend($('<img/>',{ class:'progress-image',src:settings.imageUrl }));
+    $(self.element).prepend($('<div>', { class: 'progress-bar', style: 'height: ' + self.imageHeight + 'px; width: ' + self.imageHeight + 'px;' }));
+    $(self.element).children('.progress-bar').prepend($('<div>', { class: 'pb-progress' }));
+    $(self.element).children('.progress-bar').prepend($('<div>', { class: 'pb-background' }));
+    $(self.element).find('.pb-background').prepend($('<img/>', { style: 'opacity: ' + self.backgroundOpacity + ';', class: 'background-image', src: self.backgroundImageUrl }));
+    $(self.element).find('.pb-progress').prepend($('<img/>', { class: 'progress-image', src: self.imageUrl }));
 
     self.SetPercentage = function(value) {
-        if(value > 100) value = 100;
-        if(value < 0) value = 0;
+        if (value > 100) value = 100;
+        if (value < 0) value = 0;
         var toShowAmount = (settings.imageHeight / 100) * value;
-        var toHideAmount = 100 - toShowAmount;
-        $(self.element).find('.pb-progress').height(toShowAmount).css({ top: toHideAmount+'px' });
+        var toHideAmount = (settings.imageHeight / 100) * (100 - value);
+        $(self.element).find('.pb-progress').height(toShowAmount).css({ top: toHideAmount + 'px' });
     };
 }
 
 $.fn.progressBar = function(settings) {
-    
-    settings.element = $(this);
-    var newProgressBar = ProgressBar(settings)
-    return newProgressBar;
-    
-    var gauge = $(this);
-    $(this).addClass('gauge');
-    // add the images we need; a background and a pointer
-    $(this).prepend($('<img>',{class:'pointer',src:'images/'+pointerImage+'.png', style:'top:'+pointerVerticalOffset+';', alt:'gauge pointer'}))
-    $(this).prepend($('<img>',{class:'backing',src:'images/'+backingImage+'.png', alt:'gauge background'}))
-    // animate the pointer from 0 to the target angle, then start it twitching
 
+    settings.element = $(this);
+    var newProgressBar = new ProgressBar(settings)
+    return newProgressBar;
 
 };
